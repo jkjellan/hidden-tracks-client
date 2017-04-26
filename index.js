d72b5233770405b4ae8d936419aace99714b2143
jkjellan <jkjellan@gmail.com>
2017-04-26 17:48:16 -0400

    Stores and references local copy of MDL
    I save a local copy of Material Design Lite in assets/mdl
    and reference mdl from index.scss and index.js

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ require('./assets/scripts/index.js')
 
 // styles
 require('./assets/styles/index.scss')
+
+// adds material design lite, which I store locally in assets
+require('./assets/mdl/material-design-lite/material.min.js')
